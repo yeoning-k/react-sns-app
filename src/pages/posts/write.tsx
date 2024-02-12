6d0af67d176733dc1fb060bc89bde1fe0b7258b7
yeoning <sy03549@gmail.com>
2024-02-13 02:03:43 +0900

    feat: 상단 header 에 뒤로가기 버튼 추가

--- src/pages/posts/write.tsx
+++ src/pages/posts/write.tsx
@@ -1,12 +1,18 @@
 import PostForm from 'components/posts/PostForm';
+import { FiArrowLeft } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
 
 export default function PostWritePage() {
+  const navigate = useNavigate();
   return (
-    <>
-      <div className="page-header">
+    <div className="post">
+      <div className="page-header post__header">
+        <div onClick={() => navigate(-1)}>
+          <FiArrowLeft />
+        </div>
         <h2 className="page-header__title">글쓰기</h2>
       </div>
       <PostForm />
-    </>
+    </div>
   );
 }
