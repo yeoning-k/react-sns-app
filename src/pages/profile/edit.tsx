38ed8b9f2b1c15b4e6a19a20c4cc1870c500e5c7
yeoning <sy03549@gmail.com>
2024-02-15 19:54:44 +0900

    feat: 프로필 작업

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -1,3 +1,156 @@
+import { useContext, useEffect, useState } from 'react';
+import PageHeader from '../../components/PageHeader';
+import { FiImage, FiMinus } from 'react-icons/fi';
+import AuthContext from 'context/AuthContext';
+import { v4 as uuidv4 } from 'uuid';
+import { storage } from 'firebaseApp';
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadString
+} from 'firebase/storage';
+import { updateProfile } from 'firebase/auth';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+
+const STORAGE_DOWNLOAD_URL_STRING = 'https://firebasestorage.googleapis.com/';
+
 export default function ProfileEditPage() {
-  return <>ProfileEditPage</>;
+  const [displayName, setDisplayName] = useState<string>('');
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+    const key = `${user?.uid}/${uuidv4()}`;
+    const storageRef = ref(storage, key);
+    let newImageUrl;
+
+    try {
+      if (
+        user?.photoURL &&
+        user?.photoURL?.includes(STORAGE_DOWNLOAD_URL_STRING)
+      ) {
+        const imageRef = ref(storage, user?.photoURL);
+        if (imageRef) {
+          await deleteObject(imageRef).catch(error =>
+            console.log('이미지 삭제 error', error)
+          );
+        }
+      }
+
+      if (imageUrl) {
+        const data = await uploadString(storageRef, imageUrl, 'data_url');
+        newImageUrl = await getDownloadURL(data?.ref);
+      }
+
+      if (user) {
+        await updateProfile(user, {
+          displayName: displayName || '',
+          photoURL: newImageUrl || ''
+        })
+          .then(() => {
+            toast.success('프로필 업데이트가 완료되었습니다.');
+            navigate(`/profile`);
+          })
+          .catch(e => {
+            toast.success('프로필 업데이트가 실패되었습니다.', e);
+          });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value }
+    } = e;
+
+    setDisplayName(value);
+  };
+
+  const handleFileUpload = (e: any) => {
+    const {
+      target: { files }
+    } = e;
+
+    const file = files?.[0];
+    const fileReader = new FileReader();
+    fileReader?.readAsDataURL(file);
+
+    fileReader.addEventListener('loadend', async (e: any) => {
+      const {
+        currentTarget: { result }
+      } = e;
+      setImageUrl(result);
+    });
+  };
+
+  useEffect(() => {
+    if (user?.photoURL) {
+      setImageUrl(user.photoURL);
+    }
+
+    if (user?.displayName) {
+      setDisplayName(user.displayName);
+    }
+  }, [user]);
+
+  return (
+    <div className="post">
+      <PageHeader title="프로필 수정" />
+      <form className="post-form" onSubmit={onSubmit}>
+        <div className="post-form__profile">
+          <input
+            type="text"
+            className="post-form__input"
+            name="displayName"
+            id="displayName"
+            placeholder="이름"
+            onChange={onChange}
+            value={displayName}
+          />
+        </div>
+        <div className="post-form__bottom">
+          <div className="post-form__file">
+            <label htmlFor="file-input">
+              <FiImage />
+            </label>
+            <input
+              type="file"
+              name="file-input"
+              id="file-input"
+              value=""
+              onChange={handleFileUpload}
+              accept="image/*"
+              className="hidden"
+            />
+            {imageUrl && (
+              <div className="post-form__image">
+                <img
+                  src={imageUrl}
+                  alt="attachment"
+                  width="auto"
+                  height={100}
+                />
+                <div
+                  className="post-form__image-clear-btn"
+                  onClick={() => setImageUrl(null)}
+                >
+                  <FiMinus />
+                </div>
+              </div>
+            )}
+          </div>
+          <button type="submit" className="post-form__submit">
+            프로필 수정
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,9 +1,48 @@
 import { useNavigate } from 'react-router-dom';
 import { FiUser } from 'react-icons/fi';
 import PostCard from 'components/posts/PostCard';
+import { useContext, useEffect, useState } from 'react';
+import { PostProps } from 'pages';
+import AuthContext from 'context/AuthContext';
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where
+} from 'firebase/firestore';
+import { db } from 'firebaseApp';
 
 export default function ProfilePage() {
   const navigate = useNavigate();
+
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const [activeTab, setActiveTab] = useState<'my' | 'like'>('my');
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (user) {
+      let postsRef = collection(db, 'posts');
+      let postsQuery = query(
+        postsRef,
+        activeTab === 'like'
+          ? where('likes', 'array-contains', user.uid)
+          : where('uid', '==', user.uid),
+        orderBy('createdAt', 'desc')
+      );
+
+      onSnapshot(postsQuery, snapShot => {
+        let dataObj = snapShot.docs.map(doc => {
+          return {
+            ...doc.data(),
+            id: doc?.id
+          };
+        });
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [user, activeTab]);
+
   return (
     <>
       <div className="page-header">
@@ -11,10 +50,14 @@ export default function ProfilePage() {
       </div>
       <div className="profile">
         <div className="profile__icon">
-          <FiUser />
+          {user?.photoURL ? (
+            <img src={user.photoURL} alt="user profile" />
+          ) : (
+            <FiUser />
+          )}
         </div>
-        <div className="profile__name">누구님</div>
-        <div className="profile__email">test@test.com</div>
+        <div className="profile__name">{user?.displayName ?? '사용자님'}</div>
+        <div className="profile__email">{user?.email}</div>
         <div className="profile__btn">
           <div
             className="profile__edit"
@@ -26,10 +69,26 @@ export default function ProfilePage() {
         </div>
       </div>
       <div className="tab">
-        <div className="tab__menu tab__menu--active">내가 쓴 글</div>
-        <div className="tab__menu">좋아요</div>
-        <div className="tab__bar"></div>
+        <div
+          className={`tab__menu ${activeTab === 'my' && 'tab__menu--active'}`}
+          onClick={() => setActiveTab('my')}
+        >
+          내가 쓴 글
+        </div>
+        <div
+          className={`tab__menu ${activeTab === 'like' && 'tab__menu--active'}`}
+          onClick={() => setActiveTab('like')}
+        >
+          좋아요
+        </div>
       </div>
+      {posts?.length > 0 ? (
+        posts.map(post => {
+          return <PostCard post={post} key={post.id} />;
+        })
+      ) : (
+        <div className="no-items">게시글이 없습니다 🥲</div>
+      )}
     </>
   );
 }
